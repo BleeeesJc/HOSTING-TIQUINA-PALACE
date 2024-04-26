bb9be16eb568026687eb27e60a94ffbfaa53174b
Dam <damaris.nina@ucb.edu.bo>
2024-04-26 00:01:50 -0400

    Interaz habitaciones funcionando

--- Reservas/Habitaciones.js
+++ Reservas/Habitaciones.js
@@ -1,20 +1,28 @@
-let slideIndex = 0;
-showSlides(slideIndex);
+let sliders = {
+  roomCarousel1: 0,
+  roomCarousel2: 0,
+  roomCarousel3: 0,
+  roomCarousel4: 0
+};
 
-function plusSlides(n) {
-  slideIndex += n;
-  showSlides(slideIndex);
+function plusSlides(n, carouselId) {
+  sliders[carouselId] += n;
+  showSlides(sliders[carouselId], carouselId);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("carousel-slide");
-  if (n >= slides.length) { slideIndex = 0; }
-  if (n < 0) { slideIndex = slides.length - 1; }
+function showSlides(n, carouselId) {
+  let slides = document.querySelectorAll('#' + carouselId + ' .carousel-slide');
+  if (n >= slides.length) { sliders[carouselId] = 0; }
+  if (n < 0) { sliders[carouselId] = slides.length - 1; }
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  for (let slide of slides) {
+      slide.style.display = "none";  
   }
-  
-  slides[slideIndex].style.display = "block";  
+  slides[sliders[carouselId]].style.display = "block";  
 }
+document.addEventListener('DOMContentLoaded', function() {
+  showSlides(sliders.roomCarousel1, 'roomCarousel1');
+  showSlides(sliders.roomCarousel2, 'roomCarousel2');
+  showSlides(sliders.roomCarousel3, 'roomCarousel3');
+  showSlides(sliders.roomCarousel4, 'roomCarousel4');
+});
